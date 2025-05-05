d0a4d542d9068f98b8bb45a8543fef09deeae5ce
leonidprinceton <pogorl@rpu.edu>
2025-05-05 16:16:59 -0400

    added 3D rendering and point matching

---
+++ oxels/obj_3d/__init__.py
@@ -0,0 +1,2 @@
+from .render import render, get_pose
+from .match_points import sample_random_on_mesh, get_visible_matches
---
+++ oxels/obj_3d/render.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+import trimesh
+from typing import Tuple
+
+def render(scene: trimesh.Scene, pose: np.ndarray, resolution: Tuple[int,int], f: float, visible: bool = False):
+    # set the camera
+    scene.camera_transform  = pose
+    scene.camera.resolution = resolution
+    scene.camera.focal      = (f,f)
+    
+    # render to PNG bytes
+    png_bytes = scene.save_image(resolution=resolution, visible=visible)
+
+    buf = np.frombuffer(png_bytes, dtype=np.uint8)
+    img_bgr = cv2.imdecode(buf, cv2.IMREAD_COLOR)
+    return cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)/255
+
+
+def get_pose(M: np.ndarray, d: np.ndarray):
+    pose = np.zeros((4,4))
+    pose[3,3] = 1
+    pose[:3,:3] = M
+    pose[:3,3] = d
+    return pose
