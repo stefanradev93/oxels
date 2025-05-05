0ca1a793a3dd22353f9a498da8864a1024d3d12a
LarsKue <lars@kuehmichel.de>
2025-05-05 13:33:18 -0400

    add import for DG Perspective Dataset

--- oxels/datasets/__init__.py
+++ oxels/datasets/__init__.py
@@ -1,3 +1,4 @@
+from .dg_perspective_dataset import DGPerspectiveDataset
 from .oxel_dataset import OxelDataset
 from .perspective_dataset import PerspectiveDataset
 from .analytic_dataset import AnalyticDataset
