0583c0486cfe3d6b0d77d043859169ac1dac876e
LarsKue <lars@kuehmichel.de>
2025-05-14 02:18:15 +0200

    synced logging in metrics mixin

--- oxels/models/metrics_mixin.py
+++ oxels/models/metrics_mixin.py
@@ -11,20 +11,20 @@ class MetricsMixin(LightningModule):
     def training_step(self, batch, batch_idx, dataloader_idx=0):
         metrics = self.compute_metrics(batch)
         for key, value in metrics.items():
-            self.log(f"training/{key}", value)
+            self.log(f"training/{key}", value, sync_dist=True)
 
         return metrics["loss"]
 
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
         metrics = self.compute_metrics(batch)
         for key, value in metrics.items():
-            self.log(f"validation/{key}", value)
+            self.log(f"validation/{key}", value, sync_dist=True)
 
         return metrics["loss"]
 
     def test_step(self, batch, batch_idx, dataloader_idx=0):
         metrics = self.compute_metrics(batch)
         for key, value in metrics.items():
-            self.log(f"testing/{key}", value)
+            self.log(f"testing/{key}", value, sync_dist=True)
 
         return metrics["loss"]
