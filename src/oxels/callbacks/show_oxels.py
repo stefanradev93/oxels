8ad02c5323d0ac8d2488fdf00a033c352d81b874
LarsKue <lars@kuehmichel.de>
2025-05-17 00:49:41 +0200

    fix oxel display

--- src/oxels/callbacks/show_oxels.py
+++ src/oxels/callbacks/show_oxels.py
@@ -6,18 +6,20 @@ import wandb
 class ShowOxels(L.Callback):
     def __init__(self, images: torch.Tensor, num_oxels=8):
         super().__init__()
-        self.images = images
+        self.images = torch.as_tensor(images)
         self.num_oxels = num_oxels
 
     def on_validation_epoch_end(self, trainer, pl_module):
+        images = self.images.to(pl_module.device)
         # (batch_size, num_oxels, height, width)
-        oxels = pl_module(self.images.to(pl_module.device))
+        oxels = pl_module(images)
+
         oxels = oxels[:, : self.num_oxels]
 
         batch_size, channels, height, width = oxels.shape
 
         # make a grid of shape (batch_size * height, channels * width)
-        oxels = oxels.permute(0, 2, 3, 1)
+        oxels = oxels.permute(0, 2, 1, 3)
         oxels = oxels.reshape((batch_size * height, channels * width))
 
         # ensure wandb knows these are grayscale
